8195857290bab59ed8165cec418639ab08fcced2
qiangzhili <1205131727@qq.com>
2022-08-27 21:30:05 +0800

    2022年8月27日21:30:04

---
+++ assets/middle.4185d8c9.js
@@ -0,0 +1,1 @@
+import{_ as e,g as o,o as t}from"./index.cf87c7de.js";const r={name:"Middle",mounted(){this.$router.go(-1)}};function n(s,c,a,d,_,i){return t(),o("div")}const m=e(r,[["render",n]]);export{m as default};
