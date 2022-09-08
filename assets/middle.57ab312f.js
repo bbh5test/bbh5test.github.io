d20a50f50a71e86dced646cc3e2dc3ddf6862fb6
qiangzhili <1205131727@qq.com>
2022-09-08 14:23:00 +0800

    2022年9月8日14:22:56

--- assets/middle.57ab312f.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,d as o,o as t}from"./index.b2dfe4f8.js";const r={name:"Middle",mounted(){this.$router.go(-1)}};function n(s,c,a,d,_,i){return t(),o("div")}const m=e(r,[["render",n]]);export{m as default};
