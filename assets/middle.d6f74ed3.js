23f7ee58f25fb2e9ad1521978cebeaba3c6ea1d1
caitongxue <675398066@qq.com>
2022-09-19 17:42:00 +0800

    2022年9月19日 17:41:59

---
+++ assets/middle.d6f74ed3.js
@@ -0,0 +1,1 @@
+import{_ as e,d as o,o as t}from"./index.e3b8e14c.js";const r={name:"Middle",mounted(){this.$router.go(-1)}};function n(s,c,a,d,_,i){return t(),o("div")}const m=e(r,[["render",n]]);export{m as default};
